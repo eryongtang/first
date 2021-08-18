b1bd9a743ce3ce6185764f080dd62ba9cab38934
teyong <2416367055@qq.com>
2021-08-18 16:08:27 +0800

    change 21 to 20 s1

--- test1.c
+++ test1.c
@@ -6,7 +6,7 @@ struct student{
 };
 int main(){
     struct student s={16,"张三"};
-    struct student s1 = {21,"李四"}；
+    struct student s1 = {20,"李四"}；
     printf("%d %s\n",s.age,s.name);
     return 0;
 }
